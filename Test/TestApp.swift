bdbea05c782a176e55737dd2296c9dd1499af8f8
kihec <hananmesic301@gmail.com>
2025-04-26 11:23:47 +0200

    Initial commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by kihec on 26. 4. 25.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
